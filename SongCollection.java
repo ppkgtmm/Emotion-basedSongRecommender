f0698aaad7271d73869cfaa8af3033b451dad12f
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-04-07 02:12:42 +0700

    sleepy

---
+++ SongCollection.java
@@ -0,0 +1,42 @@
+package Emotion
+
+import java.util.ArrayList;
+
+public class SongCollection
+{
+    private ArrayList<Song> songs;
+    public SongCollection()
+    {
+        songs = new ArrayList<>();
+    }
+    public ArrayList<Song> getAllSongs() {
+        return songs;
+    }
+
+    public ArrayList<Song> getSongs(String keyword) {
+        ArrayList<Song> result = new ArrayList<>();
+        for(Song song:songs)
+        {
+            if(song.getTitle().contains(keyword))
+            {
+                result.add(song);
+            }
+        }
+        return result;
+    }
+    public void addSong(Song song)
+    {
+        songs.add(song);
+    }
+    public Song getSongByID(Integer id)
+    {
+        for(Song song:songs)
+        {
+            if(song.getId().equals(id))
+            {
+                return song;
+            }
+        }
+        return null;
+    }
+}
---
+++ SongManager.java
@@ -0,0 +1,68 @@
+package Emotion
+
+import java.util.ArrayList;
+
+public class SongManager
+{
+    private SongCollection songs;
+    private static SongManager songManager= null;
+    private SongReader reader;
+
+    private SongManager()
+    {
+
+    }
+    public static SongManager getInstance()
+    {
+        if(SongManager.songManager == null)
+        {
+            songManager = new SongManager();
+        }
+        return SongManager.songManager;
+    }
+
+    public ArrayList<Song> getAllSongs()
+    {
+        return songs.getAllSongs();
+    }
+
+    public Song getASong(Integer id)
+    {
+        return songs.getSongByID(id);
+    }
+
+    public ArrayList<Song> getSongs(String keyword)
+    {
+        return songs.getSongs(keyword);
+    }
+
+    public boolean readSongs(String fileName)
+    {
+        boolean result = false;
+        reader = new SongReader();
+        songs = new SongCollection();
+        if (!reader.open(fileName))
+        {
+            System.out.println("Error opening song file "+fileName);
+            System.exit(1);
+        }
+        Song nextSong = null;
+        while ((nextSong = reader.readSong()) != null)
+        {
+            System.out.println("Successfully added " + nextSong.getTitle());
+            for (String lyric:nextSong.getLyrics())
+            {
+                System.out.println(lyric);
+            }
+                songs.addSong(nextSong);
+                result = true;
+        }
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        SongManager manager = SongManager.getInstance();
+        songManager.readSongs("songs.txt");
+    }
+
+}
