da57a678b59ed64b42bae50bb176b8a30d21c60e
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-04-13 20:33:25 +0700

    Emotions deleted

--- SongComparator.java
+++ SongComparator.java
@@ -3,6 +3,10 @@ public class SongComparator implements Comparator<Song>
 {
     private static String currentEmotion = null;
 
+    private int compareById(Song song1,Song song2)
+    {
+        return song1.getId().compareTo(song2.getId());
+    }
     @Override
     public int compare(Song song1,Song song2) {
         if(song2.getScore(currentEmotion)>song1.getScore(currentEmotion))
@@ -10,7 +14,7 @@ public class SongComparator implements Comparator<Song>
         else if(song1.getScore(currentEmotion)>song2.getScore(currentEmotion))
             return -1;
         else
-            return 0;
+            return compareById(song1,song2);
     }
 
     public static void setEmotion(String emotion)
