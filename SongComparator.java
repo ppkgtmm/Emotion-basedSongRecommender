e83ab9a2f9dd616a712ca65e8636422c13697e41
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-05-21 20:41:31 +0700

    comment

--- SongComparator.java
+++ SongComparator.java
@@ -1,29 +1,44 @@
 /**
  *  SongComparator.java
  *
- *  Simple comparator to compare emotion score
+ *  Simple comparator to compare emotion score of
+ *  songs
  *
- *  Created by Pinky Gautam , Thitiporn Sukpartcharoen, 19 May 2020
+ *  Created by
+ *  Pinky Gautam ID: 60070503401,
+ *  Thitiporn Sukpartcharoen ID: 60070503419
+ *
+ *  19 May 2020
  */
 import java.util.Comparator;
 public class SongComparator implements Comparator<Song>
 {
-    /**
-     * current emotion
-     */
+    /** current emotion used to compare the scores */
     private static String currentEmotion = null;
 
     /**
-     * 
-     * @param song1 
-     * @param song2
-     * @return
+     * compare songs by their id
+     * @param song1 first song in comparision
+     * @param song2 second song in comparision
+     * @return negative number if song2 id is less than song1,
+     * positive number if song2 id is greater that song1,
+     * otherwise return 0.
      */
     private int compareById(Song song1,Song song2)
     {
         return song2.getId().compareTo(song1.getId());
     }
 
+    /**
+     * Override compare method to compare songs based on
+     * emotion score and call other function if the scores
+     * are equal.
+     * @param song1 first song in comparision
+     * @param song2 second song in comparision
+     * @return -1 if song2 emotion score is less than song1,
+     * 1 if song2 score id is greater that song1,
+     * otherwise call function to compare songs by id.
+     */
     @Override
     public int compare(Song song1,Song song2) {
         if(song2.getScore(currentEmotion)>song1.getScore(currentEmotion))
@@ -34,6 +49,10 @@ public class SongComparator implements Comparator<Song>
             return compareById(song1,song2);
     }
 
+    /**
+     * Sets current emotion
+     * @param emotion emotion to set.
+     */
     public static void setEmotion(String emotion)
     {
         currentEmotion = emotion;
