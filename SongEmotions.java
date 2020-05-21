e6cad22dab6e12f2ae3239184ef14d3659330da4
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-05-21 21:38:58 +0700

    comment

--- SongEmotions.java
+++ SongEmotions.java
@@ -1,11 +1,15 @@
 /**
- *  SongEmotion.java
+ *  SongEmotions.java
  *
- *  This class represents organization to manage songs with emotions
- *  which has collection of songs with emotions, removed songs from
- *  category. Moreover, they are managed by this.
+ *  This class represents organization to manage songs related with
+ *  emotions like getting songs from emotion or removing song from
+ *  emotion category etc.
  *
- *  Created by Pinky Gautam , Thitiporn Sukpartcharoen, 19 May 2020
+ *  Created by
+ *  Pinky Gautam ID: 60070503401,
+ *  Thitiporn Sukpartcharoen ID: 60070503419
+ *
+ *  19 May 2020
  */
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,29 +19,29 @@ import java.util.TreeSet;
 
 public class SongEmotions
 {
-    /**
-     * songs with emotion which are sorted
+    /** to store songs related to emotions in sorted order
+     * keys : emotion category
+     * values : list of songs related
      */
     private HashMap<String,TreeSet<Song>> songsWithEmotions;
 
-    /**
-     *  constructure of songEmotions
-     */
+    /** instance of SongEmotions to for managing songs related to emotions */
     private static SongEmotions songEmotions = null;
 
-    /**
-     * removed song from category sorted
+    /** store songs removed from emotion category in the past
+     * keys : emotion category
+     * values : list of song titles that had been removed
      */
     private HashMap<String, ArrayList<String>> songsRemoved;
 
-    /**
-     * current removed song
-     */
+    /** reader that knows how to read and parse the removed song file  */
     private RemovedSongReader removedSongReader = null;
 
     /**
-     * Constructor sets the songs with emotion and removed song from category.
+     * Constructor which instantiates HashMaps that will be
+     * used later.
      */
+
     private SongEmotions()
     {
         songsWithEmotions = new HashMap<>();
@@ -45,8 +49,9 @@ public class SongEmotions
     }
 
     /**
-     * Getter for songEmotions instance
-     * @return  songEmotions
+     * Getter for songEmotions instance which is created
+     * only once.
+     * @return  songEmotions instance
      */
     public static SongEmotions getInstance()
     {
@@ -58,25 +63,28 @@ public class SongEmotions
     }
 
     /**
-     * set songsWithEmotion by reading and initial it from text file
-     * @param   fileName  removed text file
-     * @return true if successful, false if it cannot initialize
+     * Store details about songs removed from emotion by
+     * calling function to read removed song file.
+     * @param   fileName  removed song text file
+     * @return true if successful, false if error occurred
      */
     public boolean initialize(String fileName)
     {
         boolean succeed = false;
-        removedSongReader = new RemovedSongReader();        /* init removedSongReader */
+        removedSongReader = new RemovedSongReader();
+        /* trying to open file */
         if (!removedSongReader.open(fileName))
         {
             System.out.println("Error opening removed songs file " + fileName);
         }
         else
         {
+            /* details read from file which contains song title and emotion */
             RemovedSongs removedSongs;
-            /** get removed song from category from text file */
+            /* getting details from removed song file */
             while ((removedSongs = removedSongReader.readRemovedSong()) != null)
             {
-                /** put removed song from category to removed song collection */
+                /* storing details (emotion and song titles) */
                 songsRemoved.put(removedSongs.getEmotion(),removedSongs.getSongs());
             }
             succeed = true;
@@ -85,70 +93,71 @@ public class SongEmotions
     }
 
     /**
-     * Getter for songs from emotion
+     * Getter for songs related to emotion
      * @return songs in emotion category
      */
     public ArrayList<Song> getSongsFromEmotion(String emotion)
     {
         ArrayList<Song> result = null;
-        TreeSet<Song> songs = songsWithEmotions.get(emotion);   /* get songs from emotion */
+        TreeSet<Song> songs = songsWithEmotions.get(emotion);
         if(songs!=null)
         {
+            /* return ArrayList for ease of use */
             result = new ArrayList<>(songs);
         }
         return result;
     }
 
     /**
-     * remove song from category by adding the song to
-     * removedSongs.
-     * @param song      removed song
-     * @param emotion   current emotion
+     * remove song from category and add the song to
+     * removed song list.
+     * @param song      song to remove
+     * @param emotion   emotion to remove song from
      * @return true if successful, false if it cannot remove
      */
     public boolean removeFromCategory(Song song,String emotion)
     {
         boolean succeed = false;
-        TreeSet<Song> songs = songsWithEmotions.get(emotion);       /* get songs from emotion */
+        /* get songs from emotion */
+        TreeSet<Song> songs = songsWithEmotions.get(emotion);
         if(songs!=null)
         {
-            //System.out.println(songs.contains(song));
-            /* add removed song from category */
+            /* adding song to removed song list */
+            /* if other songs(s) have been removed from the emotion in the past*/
             if(songsRemoved.containsKey(emotion))
             {
-                /** add removed song from initialize emotion */
                 ArrayList<String> oldSongsList = songsRemoved.get(emotion);
                 oldSongsList.add(song.getTitle());
                 songsRemoved.put(emotion,oldSongsList);
             }
+            /* first song to be removed from the emotion */
             else
             {
-                /** add removed song from new initialize emotion */
                 ArrayList<String> newList =new ArrayList<>();
                 newList.add(song.getTitle());
                 songsRemoved.put(emotion,newList);
             }
-            /** analyze song with emotion sorted by emotion score */
+            /* set emotion that will be used to compare score */
             SongComparator.setEmotion(emotion);
-            /** remove song from song with emotions */
+            /* remove song from emotion category */
             succeed = songsWithEmotions.get(emotion).remove(song);
         }
         return succeed;
     }
 
     /**
-     * check a song is it removed from emotion in songsRemoved collection
-     * @param emotion   current emotion
-     * @param song      removed song
-     * @return true if finding, false if it cannot find removed song
+     * check if a song is removed from emotion category already
+     * @param emotion   emotion to check
+     * @param song      song to check
+     * @return true if already removed, otherwise false.
      */
     private boolean isRemovedSong(String emotion,Song song)
     {
         boolean removed = false;
-        /** check is it contain the emotion */
+        /* check if the emotion has songs that are removed */
         if(songsRemoved.containsKey(emotion))
         {
-            /** find removed song from current emotion */
+            /* find song in removed song list of emotion category */
             for(String songTitle: songsRemoved.get(emotion))
             {
                 if(song.getTitle().compareToIgnoreCase(songTitle)==0)
@@ -162,37 +171,36 @@ public class SongEmotions
     }
 
     /**
-     * updating song with emotion by calculating emotion score
-     * @param emotion   current emotions
-     * @param words     words of current emotions
-     * @param songs     arraylist of song to update
-     * @return true if finding, false if it cannot find removed song
+     * Store songs related with emotion in sorted order.
+     * @param emotion   current emotion
+     * @param words     words of current emotion
+     * @param songs     ArrayList of song to store(all songs)
      */
     public void sync(String emotion,ArrayList<String> words,ArrayList<Song> songs)
     {
-        /** analyze song with emotion sorted by emotion score */
+        /* set emotion that will be used to compare score */
         SongComparator.setEmotion(emotion);
-//        System.out.println(emotion);
-        /** analyze song to calculate score and sort by emotion score */
         for(Song song:songs)
         {
+            /* song has not been removed from emotion category */
             if(!isRemovedSong(emotion,song))
             {
+                /* to calculate emotion score */
                 song.countScore(emotion,words);
-                /** set emotion score of each song and add song to each emotion category*/
-                if(song.getScore(emotion)>0)
+                if(song.getScore(emotion)>0.4)
                 {
+                    /* emotion already exist */
                     if(songsWithEmotions.containsKey(emotion))
                     {
-                        TreeSet<Song> songsByScore = songsWithEmotions.get(emotion);    /* get song by emotion */
-                        /** add song to  arraylist*/
+                        /* gets sorted song set and add current song to the set */
+                        TreeSet<Song> songsByScore = songsWithEmotions.get(emotion);
                         songsByScore.add(song);
-                        /** add song with emotion to songsWithEmotions */
                         songsWithEmotions.put(emotion,songsByScore);
                     }
+                    /* emotion has been recently added */
                     else
                     {
-                        /** create new song set to collect */
+                        /* create a new song set then add the song */
                         TreeSet<Song> newTreeSet = new TreeSet<>(new SongComparator());
                         newTreeSet.add(song);
                         songsWithEmotions.put(emotion,newTreeSet);
@@ -202,9 +210,9 @@ public class SongEmotions
         }
     }
 
-    /**
-     * write removed song from emotion
-     * @return true if succeful, false if it cannot writing text file
+     /**
+     * write songs removed from emotion to text file
+     * @return true if successful, false if error occurred
      */
     public boolean writeRemovedSongs()
     {
@@ -213,14 +221,14 @@ public class SongEmotions
         {
             FileWriter writer = new FileWriter("removed.txt");
             ArrayList<String> emotions = new ArrayList<>(songsRemoved.keySet());
-            /** write emotion and removed song */
+            /* write emotion and removed song */
             for (String emotion:emotions)
             {
-                /** get removed song by emotion */
+                /* get removed song by emotion */
                 ArrayList<String> songs = songsRemoved.get(emotion);
-                /** write emotion */
+                /* write emotion */
                 writer.write(emotion.toLowerCase()+" : [\n");
-                /** write removed songs */
+                /* write removed songs */
                 for(String song : songs)
                 {
                     writer.write(song.toLowerCase()+"\n");
@@ -236,14 +244,5 @@ public class SongEmotions
         }
         return succeed;
 
-    }
-
-    public static void main(String[] args) {
-        SongEmotions songEmotions = SongEmotions.getInstance();
-        boolean bOk = songEmotions.initialize("removed.txt");
-        // if(bOk)
-        // {
-        //     songEmotions.writeRemovedSongs();
-        // }
     }
 }
