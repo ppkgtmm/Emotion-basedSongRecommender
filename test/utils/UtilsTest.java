c5369bf735a56b12b301d92218736faa0a91d2eb
ppkgtmm <pkgtm1998@gmail.com>
2021-03-17 19:52:25 +0700

    added unit tests

---
+++ test/utils/UtilsTest.java
@@ -0,0 +1,36 @@
+package utils;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+    @Test
+    public void testParseOption(){
+        assertEquals(123, Utils.parseOption("123 0"));
+        assertEquals(-1, Utils.parseOption("abl"));
+    }
+
+    @Test
+    public void testIsNumeric(){
+        assertTrue(Utils.isNumeric("-1.09"));
+        assertFalse(Utils.isNumeric("123a"));
+        assertFalse(Utils.isNumeric("-a1"));
+        assertFalse(Utils.isNumeric(" 123"));
+
+    }
+
+    @Test
+    public void testSetSpaces(){
+        assertEquals("123 er 45", Utils.setSpaces(" 123       er 45   "));
+    }
+
+    @Test
+    public void testIsValidChoice(){
+        assertFalse(Utils.isValidChoice(-1,9));
+        assertTrue(Utils.isValidChoice(0,9));
+        assertTrue(Utils.isValidChoice(6,9));
+        assertFalse(Utils.isValidChoice(10,9));
+    }
+}
